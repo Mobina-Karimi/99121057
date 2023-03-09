bcd535e482cb4dc48d49bdaee3aa58fb9692c20a
Mobina-Karimi <mobina.801407@gmail.com>
2023-03-09 11:31:48 -0800

    4th project with separate php file

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Lesson 3</title>
-</head>
-<body>
-    <!-- about array... -->
-    <?php
+<?php
     $books= [
         [
             "title" => "The first book",
@@ -55,26 +44,6 @@
             return true;
         }
         return false;
-    })
-    ?>
-  <!-- <h1>This is the third session coding</h1> -->
-  <h3>Book List :</h3>
-    <table class="table table-bordered table-dark">
-        <thead>
-            <th> Title </th>
-            <th> Author </th>
-            <th> Publish Date </th>
-            <th> Price </th>
-        </thead>
-        <tbody>
-            <?php foreach($filteredbook as $book): ?>
-                <tr>
-                    <?php foreach($book as $key=>$value): ?>
-                        <td><?= $value ?></td>
-                    <?php endforeach ?>    
-                </tr>    
-            <?php endforeach ?>
-        </tbody>
-    </table>
-</body>
-</html>
+    });
+    require("index.view.php");
+?>
