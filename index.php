98aca3e20a70a6eeef86b91439ab4ed10d5de23b
Mobina-Karimi <mobina.801407@gmail.com>
2023-03-09 07:28:45 -0800

    Second project with if

--- index.php
+++ index.php
@@ -4,17 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lesson 1</title>
+    <title>Lesson 2</title>
 </head>
 <body>
+    <!-- about if... -->
     <?php
-    $message= "hello world 'this is string'";
-    $name = "Mobina Karimi";
-    echo('hello world');
+    $message= "first meeting with Mobina Karimi";
+    $is_read = false;
+    // $is_read = true;
+    if($is_read)
+    {
+        $message ="First meeting with Mobina Karimi is done!";
+    }
+    else
+    {
+        $message= "I must set a meeting with Mobina Karimi";
+    }
     ?>
-    <h1>This is the first session coding</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-    <h3>Hello <?php echo($name) ?> </h3>
+    <h1>This is the second session coding</h1>
+    <h3>Hello <?= $message ?> </h3>
     
 </body>
 </html>
