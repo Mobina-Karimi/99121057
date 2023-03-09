52d8f1ee13cbe78299607755af540813bd9827d6
Mobina-Karimi <mobina.801407@gmail.com>
2023-03-09 06:24:40 -0800

    First project

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lesson 1</title>
+</head>
+<body>
+    <?php
+    $message= "hello world 'this is string'";
+    $name = "Mobina Karimi";
+    echo('hello world');
+    ?>
+    <h1>This is the first session coding</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <h3>Hello <?php echo($name) ?> </h3>
+    
+</body>
+</html>
