60404346466677067667291f27b1d4a6628934a5
Mobina-Karimi <mobina.801407@gmail.com>
2023-03-09 09:34:26 -0800

    Continuation of the third project with bootstrap

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Lesson 3</title>
 </head>
 <body>
@@ -30,28 +31,27 @@
             "price" => "350000"
         ]
     ];
-    var_dump($books);
+    // var_dump($books);
     // die();
     ?>
-    <h1>This is the third session coding</h1>
+    <!-- <h1>This is the third session coding</h1> -->
     <h3>Book List :</h3>
-    <!-- <table>
-        <?php foreach($books as $key => $value){ ?>
-            <tr>
-                <td> 
-                    <?= $key ?>
-                    <?php var_dump($value) ?>
-                </td>
-            </tr>
-        <?php }?>
-    </table> -->
-    
-    <table>
-        <?php foreach($books as $value){ ?>
-            <tr>
-                <td> <?php var_dump($value) ?> </td>
-            </tr>
-        <?php }?>
+    <table class="table table-bordered table-dark">
+        <thead>
+            <th> Title </th>
+            <th> Author </th>
+            <th> Publish Date </th>
+            <th> Price </th>
+        </thead>
+        <tbody>
+            <?php foreach($books as $book){ ?>
+                <tr>
+                    <?php foreach($book as $key => $value){ ?>
+                    <td> <?= $value ?> </td>
+                    <?php } ?>
+                </tr>
+            <?php } ?>
+        </tbody>
     </table>
 </body>
 </html>
