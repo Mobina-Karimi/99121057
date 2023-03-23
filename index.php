751471bd49cb09df80a280ff262c41d42333f227
Mobina-Karimi <mobina.801407@gmail.com>
2023-03-23 14:33:50 -0700

    5th project connection of database

--- index.php
+++ index.php
@@ -1,4 +1,32 @@
 <?php
+    function connectToDB() {
+
+        $host= 'localhost';
+        $db = 'webprogramming';
+        $user = 'postgres';
+        $password = '99121057';
+
+        try {
+            $dsn = "pgsql:host=" . $host . ";port=5432;dbname=" . $db . ";";
+            $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+            
+            if ($pdo) {
+                echo "Connected to the $db database successfully!";
+            }
+
+            return $pdo;
+        } catch (PDOException $e) {
+
+            die($e->getMessage());
+
+        } finally {
+
+            if ($pdo) {
+                $pdo = null;
+            }
+        }
+    }
+
     $books= [
         [
             "title" => "The first book",
--- index.view.php
+++ index.view.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Lesson 4</title>
+    <title>Lesson 5</title>
 </head>
 <body>
     <!-- about separate view... -->
-    <!-- <h1>This is the third session coding</h1> -->
+    <!-- <h1>This is the 5th session coding</h1> -->
   <h3>Book List :</h3>
     <table class="table table-bordered table-dark">
         <thead>
@@ -28,5 +28,10 @@
             <?php endforeach ?>
         </tbody>
     </table>
+    <div>
+        <?php 
+            $db = connectToDB();
+        ?>
+    </div>
 </body>
 </html>
